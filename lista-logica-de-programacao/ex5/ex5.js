44d17804a24e009d5010a80fbfe3fc56f465f58c
unknown <joaoleonardo9921@gmail.com>
2024-03-01 19:34:14 -0300

    ex5

--- lista-logica-de-programacao/ex5/ex5.js
+++ lista-logica-de-programacao/ex5/ex5.js
@@ -4,3 +4,9 @@
     Escreva um script para ler o custo de fábrica de um carro, a porcentagem do
     distribuidor e o imposto, e calcular e escrever o custo final ao consumidor
  */
+const custoFabrica = parseFloat(prompt("Digite o custo de fabrica do carro:"));
+const percentualDistribuidor = parseFloat(prompt("Digite o percentual do distribuidor:"));
+const impostos = parseFloat(prompt("Digite o valor dos impostos:"));
+const custoDistribuidor = custoFabrica * (percentualDistribuidor / 100);
+const custoTotal = custoFabrica + custoDistribuidor + impostos;
+document.write("O custo total do carro é: ",custoTotal);
