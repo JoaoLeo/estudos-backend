cee8f3b87e05c4df710352d38111b1a479aadd23
unknown <joaoleonardo9921@gmail.com>
2024-03-01 19:24:17 -0300

    ex4

--- lista-logica-de-programacao/ex4/ex4.js
+++ lista-logica-de-programacao/ex4/ex4.js
@@ -5,4 +5,10 @@
     escrever um script para ler o custo de fábrica de um carro, calcular e escrever o
     custo final ao consumidor.
  */
+const percDistrubuidor = 28;
+const impostos = 45;
 let custoFabrica = Number(prompt("Qual o custo"))
+let custoF2 = custoFabrica + (custoFabrica * percDistrubuidor) / 100;
+let custoF3 = custoFabrica + (custoFabrica * impostos) / 100;
+let custoFinal = custoFabrica + custoF2 + custoF3;
+document.writeln("O custo final é: ", custoFinal)
