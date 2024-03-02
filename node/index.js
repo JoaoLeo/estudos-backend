1875241ec513a7e9464c959b21790f44e1b4d75b
unknown <joaoleonardo9921@gmail.com>
2024-03-01 21:53:10 -0300

    rota default

--- node/index.js
+++ node/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.listen(8080, (e)=>{ if(e) console.log(e); else console.log("Servidor rodando na porta 8080");});
 
+app.get("/" ,(req,res)=>{
+    res.send("API rodando");
+})
+
 app.get("/hello" ,(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello World");
 })
