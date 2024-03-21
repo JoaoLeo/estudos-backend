b3ed6fff4d8a11d0d1e908204695f6a7d9d2d197
unknown <joaoleonardo9921@gmail.com>
2024-03-20 21:52:32 -0300

    aula 20-03

--- node_mongo_db/db/conn.js
+++ node_mongo_db/db/conn.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-
 async function main(){
 
     try {
-        await mongoose.connect("mongodb+srv://joaoleonardo:@cluster0.fwpgaer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect(`mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.fwpgaer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log('ok');
     } catch (error) {
         console.error(error);
