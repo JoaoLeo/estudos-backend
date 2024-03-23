bcc9214f31c41d13bdd99cac1ff55bd3042155cf
unknown <joaoleonardo9921@gmail.com>
2024-03-22 21:32:03 -0300

    aula 22-03

--- node_mongo_db/db/conn.js
+++ node_mongo_db/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function main(){
 
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.fwpgaer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        await mongoose.connect(`mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.j1xae5z.mongodb.net/ `);
         console.log('ok');
     } catch (error) {
         console.error(error);
--- node_mongo_db/routes/routes.js
+++ node_mongo_db/routes/routes.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const produto = require("../models/Produto");
 const router = express.Router();
 
 // const introducao = require("./");
 
 router.get("/",(req,res)=>{
     res.send("API rodando");
+})
+
+router.get("/produtos",async (req,res)=>{
+    res.json(await produto.find());
+})
+
+router.get("/produtos/:id",async (req,res)=>{
+    res.json(await produto.findById(req.params.id));
+})
+
+router.post("/produtos",async (req,res)=>{
+    res.json(await produto.create(req.body));
+})
+
+router.delete("/produtos/:id",async (req,res)=>{
+    res.json(await produto.findByIdAndDelete(req.params.id));
+})
+
+router.put("/produtos/:id",async (req,res)=>{
+    res.json(await produto.findByIdAndUpdate(req.params.id,req.body));
 })
 
 
