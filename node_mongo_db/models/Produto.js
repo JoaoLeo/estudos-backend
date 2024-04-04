21c957c9bfbfddc5269fb5850e42ffbbcf35a98b
unknown <joaoleonardo9921@gmail.com>
2024-04-03 21:57:42 -0300

    aula 03-04

--- node_mongo_db/models/Produto.js
+++ node_mongo_db/models/Produto.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-    nome: String,
+    nome: {
+        type: String,
+        minLength: 2
+    },
     preco: Number,
     tamanho: String,
     tipo: String,
